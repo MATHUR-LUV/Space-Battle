01ddf7bdc0fa64a97cc92e6e33078e0bba59a0d7
MATHUR-LUV <luvm1403@gmail.com>
2025-03-27 00:38:21 +0530

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main()
         DrawTextEx(font, scoreText.c_str(), {50, 40}, 34, 2, yellow);
 
         DrawTextEx(font, "HIGH-SCORE", {570, 15}, 34, 2, yellow);
-        std::string highscoreText = FormatWithLeadingZeros(game.highscore, 5);
+        string highscoreText = FormatWithLeadingZeros(game.highscore, 5);
         DrawTextEx(font, highscoreText.c_str(), {655, 40}, 34, 2, yellow);
 
         game.Draw();
